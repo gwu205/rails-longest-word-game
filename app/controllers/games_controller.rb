1cf5f9bca53930bed41984730b7f57ddb3982aa1
Geoffrey Wu <gwu20510@gmail.com>
2019-08-05 22:04:31 +0900

    longest word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,9 +8,29 @@ class GamesController < ApplicationController
 
   def score
     @answer = params[:answer]
+    @letters = params[:letters]
 
     url = 'https://wagon-dictionary.herokuapp.com'
     word_serialized = open("#{url}/#{@answer}").read
     @score = JSON.parse(word_serialized)
+
+    @word = @score["word"]
+    @found = @score["found"]
+    @length = @score["length"]
+    @match = @answer.upcase.split('').all? do |letter|
+      @letters.split.include?(letter)
+    end
+
+    if @answer.empty?
+      @message = "Please enter a word"
+    elsif @match && @found
+      @message = "Congrats! Your answer: #{@answer}"
+    elsif @match && !@found
+      @message = "#{@answer.capitalize} is not a word!"
+    elsif !@match && @found
+      @message = "Sorry! #{@answer.capitalize} is a word but not in #{@letters}"
+    else
+      @message = "Try again!"
+    end
   end
 end
